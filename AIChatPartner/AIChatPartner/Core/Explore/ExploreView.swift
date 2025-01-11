fea7e1d4deefb94220eb82a585e086fff474f87c
Javier Heisecke <javierheiseckee@gmail.com>
2025-01-11 15:31:18 -0300

    Avatar model

--- AIChatPartner/AIChatPartner/Core/Explore/ExploreView.swift
+++ AIChatPartner/AIChatPartner/Core/Explore/ExploreView.swift
@@ -8,14 +8,24 @@
 import SwiftUI
 
 struct ExploreView: View {
+
+    let avatar = AvatarModel.mock
+
     var body: some View {
         NavigationStack {
-            Text("Explore")
-                .navigationTitle("Explore")
+            HeroCellView(
+                title: avatar.name,
+                subtitle: avatar.characterDescription,
+                imageName: avatar.profileImageName
+            )
+            .frame(height: 200)
+            .navigationTitle("Explore")
         }
     }
 }
 
 #Preview {
-    ExploreView()
+    NavigationStack {
+        ExploreView()
+    }
 }
---
+++ AIChatPartner/AIChatPartner/Services/Avatar/Models/AvatarModel.swift
@@ -0,0 +1,153 @@
+//
+//  AvatarModel.swift
+//  AIChatPartner
+//
+//  Created by Javier Heisecke on 2025-01-11.
+//
+/// https://www.youtube.com/watch?v=wSmTbtOwgbE&list=PLwvDm4Vfkdphr2Dl4sY4rS9PLzPdyi8PM&index=14&t=4s
+
+import Foundation
+
+struct AvatarModel {
+
+    let avatarId: String
+    let name: String?
+    let profileImageName: String?
+    let characterOption: CharacterOption?
+    let characterAction: CharacterAction?
+    let characterLocation: CharacterLocation?
+    let authorId: String?
+    let dateCreated: Date?
+
+    init(
+        avatarId: String,
+        name: String? = nil,
+        profileImageName: String? = nil,
+        characterOption: CharacterOption? = nil,
+        characterAction: CharacterAction? = nil,
+        characterLocation: CharacterLocation? = nil,
+        authorId: String? = nil,
+        dateCreated: Date? = nil
+    ) {
+        self.avatarId = avatarId
+        self.name = name
+        self.profileImageName = profileImageName
+        self.characterOption = characterOption
+        self.characterAction = characterAction
+        self.characterLocation = characterLocation
+        self.authorId = authorId
+        self.dateCreated = dateCreated
+    }
+
+    var characterDescription: String {
+        AvatarDescriptionBuilder(avatar: self).characterDescription
+    }
+}
+
+enum CharacterOption: String {
+    case man, woman, alien, dog, cat
+
+    static var `default`: Self {
+        .man
+    }
+}
+
+enum CharacterAction: String {
+    case smiling, sitting, eating, drinking, walking, shopping, studying, working, relaxing, fighting, crying
+
+    static var `default`: Self {
+        .smiling
+    }
+}
+
+enum CharacterLocation: String {
+    case park, mall, museum, city, desert, forest, space
+
+    static var `default`: Self {
+        .park
+    }
+}
+
+struct AvatarDescriptionBuilder {
+    let characterOption: CharacterOption
+    let characterAction: CharacterAction
+    let characterLocation: CharacterLocation
+
+    init(characterOption: CharacterOption, characterAction: CharacterAction, characterLocation: CharacterLocation) {
+        self.characterOption = characterOption
+        self.characterAction = characterAction
+        self.characterLocation = characterLocation
+    }
+
+    init(avatar: AvatarModel) {
+        self.characterOption = avatar.characterOption ?? .default
+        self.characterAction = avatar.characterAction ?? .default
+        self.characterLocation = avatar.characterLocation ?? .default
+    }
+
+    var characterDescription: String {
+        "A \(characterOption.rawValue) that is \(characterAction.rawValue) in the \(characterLocation.rawValue)"
+    }
+}
+
+// MARK: - Mocks
+
+extension AvatarModel {
+    static var mocks: [AvatarModel] {
+        [
+            AvatarModel(
+                avatarId: UUID().uuidString,
+                name: "Alpha",
+                profileImageName: Constants.randomImage,
+                characterOption: .woman,
+                characterAction: .crying,
+                characterLocation: .city,
+                authorId: UUID().uuidString,
+                dateCreated: .now
+            ),
+            AvatarModel(
+                avatarId: UUID().uuidString,
+                name: "Beta",
+                profileImageName: Constants.randomImage,
+                characterOption: .man,
+                characterAction: .smiling,
+                characterLocation: .forest,
+                authorId: UUID().uuidString,
+                dateCreated: .now
+            ),
+            AvatarModel(
+                avatarId: UUID().uuidString,
+                name: "Gamma",
+                profileImageName: Constants.randomImage,
+                characterOption: .dog,
+                characterAction: .sitting,
+                characterLocation: .space,
+                authorId: UUID().uuidString,
+                dateCreated: .now
+            ),
+            AvatarModel(
+                avatarId: UUID().uuidString,
+                name: "Delta",
+                profileImageName: Constants.randomImage,
+                characterOption: .cat,
+                characterAction: .drinking,
+                characterLocation: .mall,
+                authorId: UUID().uuidString,
+                dateCreated: .now
+            ),
+        ]
+    }
+    
+    static var mock: AvatarModel {
+        AvatarModel(
+            avatarId: UUID().uuidString,
+            name: "Alpha",
+            profileImageName: Constants.randomImage,
+            characterOption: .woman,
+            characterAction: .crying,
+            characterLocation: .city,
+            authorId: UUID().uuidString,
+            dateCreated: .now
+        )
+    }
+}
