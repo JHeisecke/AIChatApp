2d77c8eec870d7575de877f7fab9d2d8efa2a533
Javier Heisecke <javierheiseckee@gmail.com>
2025-02-28 14:47:00 -0300

    [Configuration] Add Multiple Schemes

--- AIChatPartner/AIChatPartner/Core/AIChatPartnerApp.swift
+++ AIChatPartner/AIChatPartner/Core/AIChatPartnerApp.swift
@@ -8,6 +8,8 @@
 import SwiftUI
 import FirebaseCore
 
+// MARK: - AIChatPartnerApp
+
 @main
 struct AIChatPartnerApp: App {
 
@@ -25,15 +27,30 @@ struct AIChatPartnerApp: App {
     }
 }
 
+// MARK: - AppDelegate
+
 class AppDelegate: NSObject, UIApplicationDelegate {
     var dependencies: Dependencies!
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil) -> Bool {
         FirebaseApp.configure()
-        dependencies = Dependencies()
+        #if MOCK
+        dependencies = Dependencies(config: .mock(isSigedIn: true))
+        #elseif DEV
+        dependencies = Dependencies(config: .dev)
+        #else
+        dependencies = Dependencies(config: .prod)
+        #endif
         return true
     }
 }
+
+
+enum BuildConfiguration {
+    case mock(signedIn: Bool), dev, prod
+}
+
+// MARK: - Dependencies
 
 @MainActor
 struct Dependencies {
@@ -43,14 +60,32 @@ struct Dependencies {
     let avatarManager: AvatarManager!
     let chatManager: ChatManager!
 
-    init() {
-        self.authManager = AuthManager(service: FirebaseAuthService())
-        self.userManager = UserManager(service: ProductionUserServices())
-        self.aiManager = AIManager(service: OpenAIService())
-        self.avatarManager = AvatarManager(service: FirebaseAvatarService(), local: SwiftDataLocalAvatarPersistence())
-        self.chatManager = ChatManager(service: FirebaseChatService())
+    init(config: BuildConfiguration) {
+
+        switch config {
+        case .mock(let isSignedIn):
+            self.authManager = AuthManager(service: MockAuthService(user: isSignedIn ? .mock() : nil))
+            self.userManager = UserManager(service: MockUserServices(user: isSignedIn ? UserModel.mock : nil))
+            self.aiManager = AIManager(service: MockAIService())
+            self.avatarManager = AvatarManager(service: MockAvatarService(), local: MockLocalAvatarPersistance())
+            self.chatManager = ChatManager(service: MockChatService())
+        case .dev:
+            self.authManager = AuthManager(service: FirebaseAuthService())
+            self.userManager = UserManager(service: ProductionUserServices())
+            self.aiManager = AIManager(service: OpenAIService())
+            self.avatarManager = AvatarManager(service: FirebaseAvatarService(), local: SwiftDataLocalAvatarPersistence())
+            self.chatManager = ChatManager(service: FirebaseChatService())
+        case .prod:
+            self.authManager = AuthManager(service: FirebaseAuthService())
+            self.userManager = UserManager(service: ProductionUserServices())
+            self.aiManager = AIManager(service: OpenAIService())
+            self.avatarManager = AvatarManager(service: FirebaseAvatarService(), local: SwiftDataLocalAvatarPersistence())
+            self.chatManager = ChatManager(service: FirebaseChatService())
+        }
     }
 }
+
+// MARK: - View Extension
 
 extension View {
     func previewEnvironment(isSignedIn: Bool = true) -> some View {
